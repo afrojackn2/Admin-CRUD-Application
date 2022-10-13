9e0ab4675d29944652a19c2b5f753944061b2eb1
Afroj Alam <afroja1415@gmail.com>
2022-10-13 10:26:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,125 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      title: 'React Simple CRUD Application',
+      act: 0,
+      index: '',
+      datas: []
+    }
+  } 
+
+  componentDidMount(){
+    this.refs.name.focus();
+  }
+
+  fSubmit = (e) =>{
+    e.preventDefault();
+    console.log('try');
+
+    let datas = this.state.datas;
+    let name = this.refs.name.value;
+    let address = this.refs.address.value;
+
+    if(this.state.act === 0){   //new
+      let data = {
+        name, address
+      }
+      datas.push(data);
+    }else{                      //update
+      let index = this.state.index;
+      datas[index].name = name;
+      datas[index].address = address;
+    }    
+
+    this.setState({
+      datas: datas,
+      act: 0
+    });
+
+    this.refs.myForm.reset();
+    this.refs.name.focus();
+  }
+
+  fRemove = (i) => {
+    let datas = this.state.datas;
+    datas.splice(i,1);
+    this.setState({
+      datas: datas
+    });
+
+    this.refs.myForm.reset();
+    this.refs.name.focus();
+  }
+
+  fEdit = (i) => {
+    let data = this.state.datas[i];
+    this.refs.name.value = data.name;
+    this.refs.address.value = data.address;
+
+    this.setState({
+      act: 1,
+      index: i
+    });
+
+    this.refs.name.focus();
+  }  
+
+  render() {
+    let datas = this.state.datas;
+    return (
+      <>
+      <div className='Admin-CRUD-Operation-sec'>
+            <div className='Admin-CRUD-Operation'>
+              <div className='New-Project1'>
+                   <a href='#'><h3>Manage User </h3></a>
+                   <a href='#'><h3>Manage Pages </h3></a>
+                   <a href='#'><h3>Get Onboard </h3></a>
+                   <a href='#'><h3>Partner with us </h3></a>
+                   <a href='#'><h3>Contact Us </h3></a>    
+              </div>
+            </div>  
+            
+
+
+
+
+
+
+      <div className="App">
+        <h2>{this.state.title}</h2>
+        <form ref="myForm" className="myForm">
+          <input type="text" ref="name" placeholder="your name" className="formField" />
+          <input type="text" ref="address" placeholder="your address" className="formField" />
+          <button onClick={(e)=>this.fSubmit(e)} className="myButton">submit </button>
+        </form>
+        <pre>
+          {datas.map((data, i) =>
+            <li key={i} className="myList">
+              {i+1}. {data.name}, {data.address}
+              <button onClick={()=>this.fRemove(i)} className="myListButton">remove </button>
+              <button onClick={()=>this.fEdit(i)} className="myListButton">edit </button>
+            </li>
+          )}
+        </pre>
+      </div>
+
+                    
+
+
+
+
+      </div>
+      </>
+
+
+
+    );
+  }
 }
 
 export default App;
